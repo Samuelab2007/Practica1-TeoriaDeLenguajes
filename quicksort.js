98bf07b14f34c0e9ba54a5ac040ba7d74544cc0a
Samuel Acevedo Bustamante <90534570+Samuelab2007@users.noreply.github.com>
2023-02-08 17:02:33 -0500

    Create bblSort function

--- quicksort.js
+++ quicksort.js
@@ -3,6 +3,8 @@
 let barometroIf=0;
 let barometroElse=0;
 
+let barometroBubble = 0;
+
 function quicksort(datos) {
 
     if (datos.length <= 1) {
@@ -28,11 +30,53 @@ function quicksort(datos) {
     return auxiliar.concat(quicksort(izquierdo), pivote, quicksort(derecha));
 }
 
-let testVector = Array(10).fill().map(() => Math.round(Math.random() * 40)) //Vector de números positivos aleatorizados
+// Creating the bblSort function
+function bblSort(arr){
+    
+    for(let i = 0; i < arr.length; i++){
+       
+      // Last i elements are already in place 
+      for(let j = 0; j < ( arr.length - i -1 ); j++){
+        barometroBubble++;  //Barómetro       
+        // Checking if the item at present iteration
+        // is greater than the next iteration
+        if(arr[j] > arr[j+1]){
+           
+          // If the condition is true then swap them
+          let temp = arr[j]
+          arr[j] = arr[j + 1]
+          arr[j+1] = temp
+
+        }
+      }
+    }
+    // Print the sorted array
+    return arr;
+}
+     
+
+   
+
+let testVector = Array(100).fill().map(() => Math.round(Math.random() * 120)); //Vector de números positivos aleatorizadose
+let copiaVector = [...testVector];
+
+
+console.log("Vector de prueba(Desordenado)");
 console.log(testVector);
 
 
+
 let resultado = quicksort(testVector);
+
+console.log("Vector ordenado por bubble sort");
+
+let ordenBubbleSort = bblSort(copiaVector); //Ordenamiento bubble Sort
+console.log(ordenBubbleSort);
+console.log("Barómetro Bubble", barometroBubble)
+
+console.log("Barómetros para quicksort");
 console.log("Cantidad de veces que se divide por la izquierda", barometroIf);
 console.log("Cantidad de veces que se divide por la derecha", barometroElse);
+
+console.log("Vector ordenado por quicksort");
 console.log(resultado);
